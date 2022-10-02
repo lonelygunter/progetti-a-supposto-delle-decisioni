5a47ddde9bfb8ec99f3bc080833995ef63d09d13
lonelygunter <aprile.matteo01@gmail.com>
2022-10-02 13:12:38 +0200

    update code berr-game
    added function, comments and variable

--- beer-game/beer-game.py
+++ beer-game/beer-game.py
@@ -1,62 +1,49 @@
-from numpy import amax
-import beer_demand
+import beer_demand as bd
 
 # default var
-d_o_cost = 100
-d_oos_cost = 1
-d_i_cost = 1
-d_inventory_initial = 540
-lead_time = 2
+d_o_cost = 100 # default variable of order cost
+d_oos_cost = 1 # default variable of out of stock cost
+d_i_cost = 1 # default variable of inventory cost
+d_initial_inventory = 540 # default variable of initial inventory pallets
+lead_time = 2 # default variable of lead time
 
 # var
-amount_supp = 0
-backlog = 0
-inv_level = d_inventory_initial
-order = 0
-i_cost = 0
-oos_cost = 0
-o_cost = 0
-arr_1 = 0
-arr_2 = 0
-bool_2_week = False
-tot_cost = 0
-curr_cost = 0
+demand = 0 # variable of demand
+amount_supp = 0 # variable of amount supplied to customers this week
+backlog = 0 # variable of backlog
+inv_level = d_initial_inventory # variable of inventory level
+order = 0 # variable of order
+i_cost = 0 # variable of inventory cost
+oos_cost = 0 # variable of out of stock cost
+o_cost = 0 # variable of order cost
+arr_1 = 0 # variable of goods arriving in 1 week
+arr_2 = 0 # variable of goods arriving in 2 week
+tot_cost = 0 # variable of total cost this week
+curr_cost = 0 # variable of cumulative total cost
 
-for i in range(0, 20):
-    print("-------------------------\n" + "| WEEK " + str(i+1) + "\n-------------------------")
-    amount_supp = beer_demand.get_demand(i)
-    o_cost = 0
-    inv_level = inv_level - amount_supp + arr_1
-
-    if amount_supp < inv_level:
-        print("| Amount supplied..| " + str(amount_supp))
-    else:
-        backlog = amount_supp - inv_level
-        print("| Amount supplied..| " + str(amount_supp - inv_level))
-    
+# function to print all data usefull befor take the order
+def preorder(i, demand, amount_supp, backlog, inv_level):
+    print("-------------------------\n" + "| WEEK " + str(i + 1) + "\n-------------------------")
+    print("| Demand...........| " + str(demand))
+    print("| Amount supplied..| " + str(amount_supp))
     print("| backlog..........| " + str(backlog))
     print("| inv level........| " + str(inv_level))
-    
 
+# function to take the order and make a little check
+def takeorder():
+    while True:
+        try:
+            order = int(input("| ?................| "))
+            break
+        except KeyboardInterrupt:
+            exit(1)
+        except:
+            print("| Only Integers....|")
 
-    order = int(input("| ?................| "))
+    return order
 
-    i_cost = inv_level * d_i_cost
-    oos_cost = backlog * d_oos_cost
-
-    if bool_2_week == True:
-        arr_1 = arr_2
-        arr_2 = 0
-
-    if order != 0:
-        o_cost = 100
-        arr_2 = order
-        bool_2_week = True
-
-    tot_cost = i_cost + oos_cost + o_cost
-    curr_cost = curr_cost + tot_cost
-    
-    
+# function to print all data usefull after taked the order
+def postorder(i_cost, oos_cost, o_cost, arr_1, arr_2, tot_cost, curr_cost):
     print("| inv cost.........| " + str(i_cost))
     print("| oos cost.........| " + str(oos_cost))
     print("| order cost.......| " + str(o_cost))
@@ -65,3 +52,41 @@ for i in range(0, 20):
     print("| tot cost.........| " + str(tot_cost))
     print("| curr cost........| " + str(curr_cost))
     print("-------------------------\n")
+
+
+# for cycle to iterate weekly demand
+for i in range(0, 20):
+    # set initial variable of amount_supp, demand and inv_level
+    demand = bd.get_demand(i)
+    inv_level = inv_level + arr_1
+
+    if demand < inv_level:
+        amount_supp = demand
+    else:
+        backlog = demand - inv_level
+        amount_supp = inv_level
+
+    inv_level = inv_level - amount_supp
+
+    # print and take order
+    preorder(i, demand, amount_supp, backlog, inv_level)
+    order = takeorder()
+
+    # set cost variable and lead time
+    i_cost = inv_level * d_i_cost
+    oos_cost = backlog * d_oos_cost
+
+    # track lead time
+    arr_1 = arr_2
+    arr_2 = order
+
+    if order != 0:
+        o_cost = 100
+    else:
+        o_cost = 0
+
+    tot_cost = i_cost + oos_cost + o_cost
+    curr_cost = curr_cost + tot_cost
+
+    # print all weekly data
+    postorder(i_cost, oos_cost, o_cost, arr_1, arr_2, tot_cost, curr_cost)
